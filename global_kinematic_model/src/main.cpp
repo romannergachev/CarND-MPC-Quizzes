f57a72c1468a23b401cc77a5d0cb84aa3f8b67ca
Roman Nergachev <roman.nergachev@zalando.fi>
2017-05-13 10:17:48 +0300

    feat: Implement quizes

--- global_kinematic_model/src/main.cpp
+++ global_kinematic_model/src/main.cpp
@@ -12,7 +12,6 @@ double rad2deg(double x) { return x * 180 / pi(); }
 
 const double Lf = 2;
 
-// TODO: Implement the global kinematic model.
 // Return the next state.
 //
 // NOTE: state is [x, y, psi, v]
@@ -20,6 +19,14 @@ const double Lf = 2;
 Eigen::VectorXd globalKinematic(Eigen::VectorXd state,
                                 Eigen::VectorXd actuators, double dt) {
   Eigen::VectorXd next_state(state.size());
+  double x   = state[0];
+  double y   = state[1];
+  double psi = state[2];
+  double v   = state[3];
+  next_state[0] = x + v * cos(psi) * dt;
+  next_state[1] = y + v * sin(psi) * dt;
+  next_state[2] = psi + v / Lf * actuators[0] * dt;
+  next_state[3] = v + actuators[1] * dt;
   return next_state;
 }
 
